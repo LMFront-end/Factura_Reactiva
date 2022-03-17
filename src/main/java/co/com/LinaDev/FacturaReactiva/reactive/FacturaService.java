8407121d4d6bf37a33a7105617869bd7239ccbd5
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-16 23:50:10 -0500

    Fix bugs

--- src/main/java/co/com/LinaDev/FacturaReactiva/reactive/FacturaService.java
+++ src/main/java/co/com/LinaDev/FacturaReactiva/reactive/FacturaService.java
@@ -1,16 +1,27 @@
 package co.com.LinaDev.FacturaReactiva.reactive;
 
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
 import java.util.List;
 
+@NoArgsConstructor
 public class FacturaService {
 
     static Data data = new Data();
     static List<Factura> listaProducto = data.listaProducto;
 
+    private static final Logger log = LoggerFactory.getLogger(FacturaService.class);
+
     // mostrar todos los elementos de la lista
     public static void mostrarListaProductos(){
         System.out.println("Mostrar todos los productos: ");
-
+        Flux.fromIterable(listaProducto)
+                .subscribe(producto -> log.info(producto.toString()));
 
     }
+
+    //
 }
