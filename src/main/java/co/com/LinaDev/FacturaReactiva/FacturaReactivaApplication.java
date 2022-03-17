76ead46f871edb4ad3acbbc48ee5b097f64d6811
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-16 23:30:02 -0500

    se crea Clase FacturaReactive

--- src/main/java/co/com/LinaDev/FacturaReactiva/FacturaReactivaApplication.java
+++ src/main/java/co/com/LinaDev/FacturaReactiva/FacturaReactivaApplication.java
@@ -1,24 +1,13 @@
 package co.com.LinaDev.FacturaReactiva;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacturaReactivaApplication implements CommandLineRunner {
-
-	// revisar a modo de debug
-	private static final Logger log = LoggerFactory.getLogger(FacturaReactivaApplication.class);
-
+public class FacturaReactivaApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacturaReactivaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }
---
+++ src/main/java/co/com/LinaDev/FacturaReactiva/reactive/FacturaReactive.java
@@ -0,0 +1,19 @@
+package co.com.LinaDev.FacturaReactiva.reactive;
+
+import co.com.LinaDev.FacturaReactiva.FacturaReactivaApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class FacturaReactive  implements CommandLineRunner {
+
+    // revisar a modo de debug
+    private static final Logger log = LoggerFactory.getLogger(FacturaReactivaApplication.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        
+
+    }
+}
